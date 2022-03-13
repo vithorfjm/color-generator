e46b011c7b3357fdee244f3a7581b44bf843252a
vithorfjm <vithorfjm@outlook.com>
2022-03-13 17:15:35 -0300

    Pequenas alterações de estilo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.querySelector("button").addEventListener("click", generateColor);
+document.querySelector('#random').addEventListener('click', generateColor);
 // window.onload = generateColor
 
 function generateColor () {
@@ -7,11 +7,11 @@ function generateColor () {
     let blue = generateNum();
     let rgbCode = `rgb(${red}, ${green}, ${blue})`;
     document.querySelector('main').style.backgroundColor = rgbCode;
-    document.querySelector('#rgb').innerHTML = rgbCode
+    document.querySelector('#rgb').innerHTML = rgbCode;
     document.querySelector('#random').style.setProperty('--button-color', rgbCode); // Deixa a cor do "Random" igual a do fundo quando passa o mouse por cima
     document.querySelector('a').style.color = rgbCode;
 }   
 
 const generateNum = () => {
-    return Math.floor(Math.random() * 256)
+    return Math.floor(Math.random() * 256);
 }
