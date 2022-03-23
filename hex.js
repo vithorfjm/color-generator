0c15fb8c0503449edcf50d2e3917af13c0b5b500
vithorfjm <vithorfjm@outlook.com>
2022-03-23 15:55:40 -0300

    adicionado gerador de cor Hexadecimal

---
+++ hex.js
@@ -0,0 +1,20 @@
+/* Mudar com Hex */
+
+document.querySelector('#random').addEventListener('click', () => {
+    const red = generateHexNum();
+    const green = generateHexNum();
+    const blue = generateHexNum();
+    const hexCode = `#${red}${green}${blue}`;
+    document.querySelector('main').style.backgroundColor = hexCode;
+    document.querySelector('#hex').innerHTML = hexCode
+    document.querySelector('#random').style.setProperty('--button-color', hexCode);
+    document.querySelector('#name').style.color = hexCode
+});
+
+/* Geradores dos números isolados */
+
+function generateHexNum () {
+    const values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+    const num = `${values[Math.floor(Math.random() * values.length)]}${values[Math.floor(Math.random() * values.length)]}`;
+    return `${num}`;
+}
---
+++ rgb.js
@@ -0,0 +1,18 @@
+/* Mudar cor RGB */
+
+document.querySelector('#random').addEventListener('click', () => {
+    const red = generateNum();
+    const green = generateNum();
+    const blue = generateNum();
+    const rgbCode = `rgb(${red}, ${green}, ${blue})`;
+    document.querySelector('main').style.backgroundColor = rgbCode;
+    document.querySelector('#rgb').innerHTML = rgbCode;
+    document.querySelector('#random').style.setProperty('--button-color', rgbCode); // Deixa a cor do "Random" igual a do fundo quando passa o mouse por cima
+    document.querySelector('#name').style.color = rgbCode;
+});
+
+/* Gerar número para cada cor do RGB */
+
+function generateNum () {
+    return Math.floor(Math.random() * 256);
+}
